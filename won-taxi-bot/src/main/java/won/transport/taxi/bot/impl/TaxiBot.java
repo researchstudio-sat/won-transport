f097134813d84a1e4d5df25e6c05e8d69b6bf35b
quasarchimaere <sudafabian@gmail.com>
2017-04-19 12:36:26 +0200

    -refactored code according to api change

--- won-taxi-bot/src/main/java/won/transport/taxi/bot/impl/TaxiBot.java
+++ won-taxi-bot/src/main/java/won/transport/taxi/bot/impl/TaxiBot.java
@@ -16,87 +16,75 @@
 
 package won.transport.taxi.bot.impl;
 
-import won.bot.framework.bot.base.EventBot;
+import won.bot.framework.bot.base.FactoryBot;
 import won.bot.framework.eventbot.EventListenerContext;
 import won.bot.framework.eventbot.action.impl.MultipleActions;
 import won.bot.framework.eventbot.action.impl.needlifecycle.DeactivateNeedAction;
 import won.bot.framework.eventbot.action.impl.wonmessage.execCommand.ExecuteConnectCommandAction;
 import won.bot.framework.eventbot.bus.EventBus;
 import won.bot.framework.eventbot.event.impl.command.connect.ConnectCommandEvent;
-import won.bot.framework.eventbot.event.impl.lifecycle.InitializeEvent;
+import won.bot.framework.eventbot.event.impl.factory.FactoryHintEvent;
 import won.bot.framework.eventbot.event.impl.wonmessage.CloseFromOtherNeedEvent;
-import won.bot.framework.eventbot.event.impl.wonmessage.HintFromMatcherEvent;
 import won.bot.framework.eventbot.event.impl.wonmessage.MessageFromOtherNeedEvent;
 import won.bot.framework.eventbot.event.impl.wonmessage.OpenFromOtherNeedEvent;
 import won.bot.framework.eventbot.listener.impl.ActionOnEventListener;
 import won.transport.taxi.bot.action.*;
 import won.transport.taxi.bot.client.MobileBooking;
-import won.transport.taxi.bot.event.FactoryHintEvent;
 
 /**
  * Created by fsuda on 27.02.2017.
  */
-public class TaxiBot extends EventBot{
-    private static final String NAME_NEEDS = "taxiNeeds";
-    private static final String NAME_FACTORYNEEDS = "taxiBotFactoryNeeds";
-
+public class TaxiBot extends FactoryBot {
     private EventBus bus;
     private MobileBooking mobileBooking;
 
-    @Override
-    protected void initializeEventListeners() {
+    protected void initializeFactoryEventListeners() {
         EventListenerContext ctx = getEventListenerContext();
         bus = getEventBus();
-
-        bus.subscribe(HintFromMatcherEvent.class,
-            new ActionOnEventListener(ctx,
-                "HintReceived",
-                new FactoryHintCheckAction(ctx, NAME_FACTORYNEEDS)
-            ));
-
-        bus.subscribe(InitializeEvent.class,
-            new ActionOnEventListener(
-                ctx,
-                "InitTaxiBot",
-                new InitFactoryAction(ctx, NAME_FACTORYNEEDS, NAME_NEEDS)
-            ));
 
         bus.subscribe(FactoryHintEvent.class,
             new ActionOnEventListener(
                 ctx,
                 "FactoryHintEvent",
-                new CreateFactoryOfferAction(ctx, mobileBooking, NAME_NEEDS)
-            ));
+                new CreateFactoryOfferAction(ctx)
+            )
+        );
 
         bus.subscribe(ConnectCommandEvent.class,
             new ActionOnEventListener(
                 ctx,
                 "FactoryOfferCreatedEvent",
                 new ExecuteConnectCommandAction(ctx)
-            ));
+            )
+        );
 
         bus.subscribe(MessageFromOtherNeedEvent.class,
             new ActionOnEventListener(
                 ctx,
                 "MessageReceived",
                 null//TODO: ADD ACTION
-            ));
+            )
+        );
 
         bus.subscribe(OpenFromOtherNeedEvent.class,
             new ActionOnEventListener(
                 ctx,
                 "FactoryOfferOpened",
-                new OpenedFactoryOfferAction(ctx, NAME_FACTORYNEEDS)
-            ));
+                new OpenedFactoryOfferAction(ctx)
+            )
+        );
 
         bus.subscribe(CloseFromOtherNeedEvent.class,
             new ActionOnEventListener(
                 ctx,
                     "FactoryOfferClosed",
-                    new MultipleActions(ctx,
+                    new MultipleActions(
+                        ctx,
                         new DeactivateNeedAction(ctx)/*,
-                        //TODO CALL ACTION TO CANCEL ORDER*/)
-            ));
+                        //TODO CALL ACTION TO CANCEL ORDER*/
+                    )
+            )
+        );
     }
 
     // ******* SETTER**********
