38eac8d708664e5e0912e4f2994f5d306cf7d2f6
quasarchimaere <fabian.suda@researchstudio.at>
2017-11-30 14:22:09 +0100

    implements first real createOrder api call and response as a textmessage

--- won-taxi-bot/src/main/java/won/transport/taxi/bot/action/ExecuteTaxiOrderAction.java
+++ won-taxi-bot/src/main/java/won/transport/taxi/bot/action/ExecuteTaxiOrderAction.java
@@ -25,7 +25,11 @@ import won.bot.framework.eventbot.event.impl.command.connectionmessage.Connectio
 import won.bot.framework.eventbot.listener.EventListener;
 import won.protocol.model.Connection;
 import won.protocol.util.WonRdfUtils;
+import won.transport.taxi.bot.client.entity.Parameter.*;
+import won.transport.taxi.bot.client.entity.Parameter.Error;
+import won.transport.taxi.bot.client.entity.Result;
 import won.transport.taxi.bot.impl.TaxiBotContextWrapper;
+import won.transport.taxi.bot.service.InformationExtractor;
 
 /**
  * Created by fsuda on 08.05.2017.
@@ -40,10 +44,38 @@ public class ExecuteTaxiOrderAction extends BaseEventBotAction {
         EventListenerContext ctx = getEventListenerContext();
 
         if(ctx.getBotContextWrapper() instanceof TaxiBotContextWrapper && event instanceof ProposalAcceptedEvent) {
-            //TODO: CONFIRM TAXI ORDER WITH MOBILEBOOKING INTERFACE SEND CONFIRM MESSAGE
             Connection con = ((ProposalAcceptedEvent) event).getCon();
-            Model messageModel = WonRdfUtils.MessageUtils.textMessage("TAXI ORDER CONFIRMED IT WILL ARRIVE AT SOME TIME"); //TODO: Change Message
 
+            TaxiBotContextWrapper taxiBotContextWrapper = (TaxiBotContextWrapper) ctx.getBotContextWrapper();
+
+            DepartureAdress departureAdress = InformationExtractor.getDepartureAdress(((ProposalAcceptedEvent) event).getPayload());
+            DestinationAdress destinationAdress = InformationExtractor.getDestinationAdress(((ProposalAcceptedEvent) event).getPayload());
+
+            Result createOrderResponse = taxiBotContextWrapper.getMobileBooking().createOrder(departureAdress, destinationAdress);
+            //TODO: SAFE ORDER NUMBER WITH CONNECTION URI FOR LATER USE (e.g. checkups and cancelations or stuff)
+            //TODO: FIGURE OUT HOW TO HANDLE MULTIPLE ORDERS (BLOCK IF ORDER ALREADY EXISTS)
+
+            String respondWith = "";
+
+            if(createOrderResponse.getError() != null) {
+                //TODO: ERROR CASES
+                Error error = createOrderResponse.getError();
+                respondWith = "ErrorID:"+ error.getErrorId() + " Text: "+ error.getText();
+            }else {
+                respondWith = "Ride from " + departureAdress + " to " + destinationAdress + ": ";
+                for (Parameter param : createOrderResponse.getParameter()) {
+                    if (param instanceof OrderId) {
+                        respondWith = respondWith + "Your OrderId is: " + ((OrderId) param).getOrderId() + ";";
+                    } else if (param instanceof DisplayText) {
+                        respondWith = respondWith + ((DisplayText) param).getText();
+                    } else if (param instanceof Price) {
+                        respondWith = respondWith + " for a price of:" + ((Price) param).getAmount() + " " + ((Price) param).getCurrency();
+                    }
+                }
+                respondWith += "....Get into the Taxi when it arrives!";
+            }
+
+            Model messageModel = WonRdfUtils.MessageUtils.textMessage(respondWith);
             getEventListenerContext().getEventBus().publish(new ConnectionMessageCommandEvent(con, messageModel));
         }
     }
