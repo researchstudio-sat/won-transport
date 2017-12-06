0f9b3aaf4e146385ce30ade3ee1361876c52df8f
quasarchimaere <fabian.suda@researchstudio.at>
2017-12-06 13:05:39 +0100

    adds msgId to function.class, and allows following new parameter-classes in the parameterlist:SERVICEID, VEHICLEID, STATE, POSTCODE, ONLY_FREE, AREA

--- won-taxi-bot/src/main/java/won/transport/taxi/bot/client/entity/Function.java
+++ won-taxi-bot/src/main/java/won/transport/taxi/bot/client/entity/Function.java
@@ -15,12 +15,18 @@ public class Function implements Serializable{
         this.name = name;
     }
 
-    public Function(String name, List<Parameter> parameter) {
+    public Function(String name, Integer msgId) {
         this(name);
+        this.msgId = msgId;
+    }
+
+    public Function(String name, Integer msgId, List<Parameter> parameter) {
+        this(name, msgId);
         this.parameter = parameter;
     }
 
     private String name;
+    private Integer msgId;
     private List<Parameter> parameter;
 
     @XmlAttribute(name="NAME")
@@ -32,13 +38,29 @@ public class Function implements Serializable{
         this.name = name;
     }
 
+    @XmlAttribute(name="MSGID")
+    public Integer getMsgId() {
+        return msgId;
+    }
+
+    public void setMsgId(Integer msgId) {
+        this.msgId = msgId;
+    }
+
     @XmlElementWrapper(name = "PARAMETER")
     @XmlElements({
             @XmlElement(name="ORDERTYPE", type=OrderType.class),
             @XmlElement(name="DEPARTUREADRESS", type=DepartureAdress.class),
             @XmlElement(name="DESTINATIONADRESS", type=DestinationAdress.class),
             @XmlElement(name="ORDERID", type=OrderId.class),
-            @XmlElement(name="TEST", type=Test.class)
+            @XmlElement(name="SERVICEID", type=ServiceId.class),
+            @XmlElement(name="VEHICLEID", type=VehicleId.class),
+            //@XmlElement(name="TEXT", type=Text.class), //TODO: CANT INCLUDE THIS (USED FOR SENDTEXTMESSAGE) BECAUSE OF NAME COLLISION WITH "TEXT" Attribute in adresses
+            @XmlElement(name="TEST", type=Test.class),
+            @XmlElement(name="STATE", type=State.class),
+            @XmlElement(name="POSTCODE", type=PostCode.class),
+            @XmlElement(name="ONLY_FREE", type=OnlyFree.class),
+            @XmlElement(name="AREA", type=Area.class)
     })
     public List<Parameter> getParameter() {
         return parameter;
